cbaec13a614975d5459deb929a17726de03aca7b
adouch9 <mr.ouldmoussa@gmail.com>
2021-03-31 22:03:03 +0200

    exo

--- append.js
+++ append.js
@@ -9,12 +9,63 @@ if (process.argv.length < 3){
 
 let commandeArgv = process.argv.slice(2, -1)
 
-for(let i = 2; i < commandeArgv.length; i++ ){
-
-    if(!fs.existsSync(commandeArgv[i])){
-        console.log(chalk.yellow(`Your ${commandeArgv} is not exist`))
-        process.exit(1)
-    }
-
-
+for (const elem of commandeArgv) {
+    if(!fs.existsSync(elem)){
+                 console.log(chalk.yellow(`Your ${elem} is not exist`))
+                 process.exit(1)
+             }
+             const stats = fs.statSync(elem);
+             if (!stats.isFile()) {
+               console.log(chalk.blue(`warning : ${elem} pas un fichier.`));
+               process.exit(1);
+             }
+             let contentFile = '';
+//length -1 pour select le dernier
+for (let i = 2; i < process.argv.length - 1; ++i) {
+  contentFile += fs.readFileSync(process.argv[i], 'utf-8') + '\n';
 }
+const last = process.argv[process.argv.length - 1];
+fs.writeFileSync(last, contentFile);
+      
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(let i = 2; i < process.argv.length -1; i++ ){
+
+//     if(!fs.existsSync(process.argv[i])){
+//         console.log(chalk.yellow(`Your ${process.argv[i]} is not exist`))
+//         process.exit(1)
+//     }
+// }
+
+// for(let i = 0; i < commandeArgv; i++ ){
+
+//     if(!fs.existsSync(process.argv[i + 2])){
+//         console.log(chalk.yellow(`Your ${process.argv[i + 2]} is not exist`))
+//         process.exit(1)
+//     }
+// }
